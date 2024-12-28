c1c913b31a7b7a56025eec1f4ec9c52560cade67
Dana Ilnytska <volodere3@gmail.com>
2024-12-28 09:02:55 +0100

    first commit

---
+++ src/Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+    public static void testA() {
+        System.out.println("Hello Word");
+    }
+
+    public static void testB() {
+        System.out.println("Test B");
+    }
+}
